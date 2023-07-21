76f95c990af459e56f449c7b5799501ba89e5a88
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-07-21 18:39:10 +0100

    creating the interactivity of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
-function Square() {
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+	return (
+		<button className="square" onClick={onSquareClick}>
+			{value}
+		</button>
+	);
+}
+
+function Board() {
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	
+	function handleClick(i) {
+		const nextSquares = squares.slice();
+		nextSquares[i] = "X";
+		setSquares(nextSquares);
+	}
+
 	return (
 		<>
 			<div className="board-row">
-				<button className="square">X</button>
-				<button className="square">X</button>
-				<button className="square">X</button>
+				<Square value={squares[0] onSquareClick {handleClick()}} />
+				<Square value={squares[1]} />
+				<Square value={squares[2]} />
 			</div>
 			<div className="board-row">
-				<button className="square">X</button>
-				<button className="square">X</button>
-				<button className="square">X</button>
+				<Square value={squares[3]} />
+				<Square value={squares[4]} />
+				<Square value={squares[5]} />
 			</div>
 			<div className="board-row">
-				<button className="square">X</button>
-				<button className="square">X</button>
-				<button className="square">X</button>
+				<Square value={squares[6]} />
+				<Square value={squares[7]} />
+				<Square value={squares[8]} />
 			</div>
 		</>
 	);
 }
-export default Square;
+export default Board;
