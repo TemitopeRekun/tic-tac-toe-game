98e638e4db25aa3aad5be8fa8aac16064d791d74
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-07-21 19:46:46 +0100

    declaring a winner

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,60 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board() {
+	const [xIsNext, setXIsNext] = useState(true);
 	const [squares, setSquares] = useState(Array(9).fill(null));
-	
+
 	function handleClick(i) {
+		if (squares[i]) {
+			return;
+		}
 		const nextSquares = squares.slice();
-		nextSquares[i] = "X";
+
+		if (xIsNext) {
+			nextSquares[i] = "X";
+		} else {
+			nextSquares[i] = "O";
+		}
+
 		setSquares(nextSquares);
+		setXIsNext(!xIsNext);
 	}
 
 	return (
 		<>
 			<div className="board-row">
-				<Square value={squares[0] onSquareClick {handleClick()}} />
-				<Square value={squares[1]} />
-				<Square value={squares[2]} />
+				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
 			</div>
 			<div className="board-row">
-				<Square value={squares[3]} />
-				<Square value={squares[4]} />
-				<Square value={squares[5]} />
+				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
 			</div>
 			<div className="board-row">
-				<Square value={squares[6]} />
-				<Square value={squares[7]} />
-				<Square value={squares[8]} />
+				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
 			</div>
 		</>
 	);
 }
 export default Board;
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squ)
+	}
+}
