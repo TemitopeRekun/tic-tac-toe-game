e1c40170ce5391d4ecbeb91ea37122373124c51a
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-07-21 20:23:35 +0100

    showing past moves

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,9 @@ function Board() {
 	);
 }
 function Game() {
+	const [xIsNext, setXIsNext] = useState(true);
+	const [history, setHistory] = useState(Array(9).fill(null));
+	const currentSquares = history[History.length - 1];
 	return (
 		<div className="game">
 			<div className="game-board">
